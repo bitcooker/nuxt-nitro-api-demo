657879b8a889f27d670ca190773c98e5bcfbff27
bitcooker <dreamhighpassionhigh@gmail.com>
2021-02-26 23:19:24 +0200

    tw hack

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,4 +2,9 @@ export default {
   target: "static",
 
   buildModules: ["@nuxtjs/tailwindcss", "@nuxt/nitro/compat"],
+
+  tailwindcss: {
+    cssPath: '~/assets/tailwind.css',
+    viewer: false
+  }
 };
--- pages/index.vue
+++ pages/index.vue
@@ -55,3 +55,9 @@ export default {
   }
 }
 </script>
+
+<style lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
