e4f88d35b738c16c779383462a9714d46b1d26a0
bitcooker <dreamhighpassionhigh@gmail.com>
2021-02-26 22:43:43 +0200

    pagination

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="bg-gray-300 py-16 px-4">
+  <div class="bg-gray-300 min-h-screen py-16 px-4">
     <div class="mx-auto container max-w-lg space-y-4">
       <div
-        v-for="quote in quotes"
+        v-for="quote in quotes.data"
         :key="quote.quote"
         class=""
       >
@@ -13,16 +13,44 @@
 
       </div>
     </div>
+
+    <div class="flex justify-center mt-4">
+      <button class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50" @click="prev">Prev</button>
+      <button class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50" @click="next">Next</button>
+    </div>
   </div>
 </template>
 
 <script>
+async function fetchQuotes(params) {
+  return await $fetch('/api/quotes', { params })
+}
+
 export default {
   async asyncData() {
-    const quotes = await $fetch('/api/quotes')
+    const quotes = await fetchQuotes({
+      page: 1
+    })
 
     return {
-      quotes
+      quotes,
+      page: 1
+    }
+  },
+  methods: {
+    async prev() {
+      if (this.page === 1) {
+        return
+      }
+
+      this.quotes = await fetchQuotes({
+        page: (--this.page)
+      })
+    },
+    async next() {
+      this.quotes = await fetchQuotes({
+        page: (++this.page)
+      })
     }
   }
 }
